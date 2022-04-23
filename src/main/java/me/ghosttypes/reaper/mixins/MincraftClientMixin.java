5bd22ef992305417b667dc424be884caea82207c
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-04-23 14:52:40 +0200

    remove icon/title overwrites

--- src/main/java/me/ghosttypes/reaper/mixins/MincraftClientMixin.java
+++ src/main/java/me/ghosttypes/reaper/mixins/MincraftClientMixin.java
@@ -1,9 +1,7 @@
 package me.ghosttypes.reaper.mixins;
 
-import me.ghosttypes.reaper.Reaper;
 import me.ghosttypes.reaper.events.InteractEvent;
 import me.ghosttypes.reaper.modules.misc.MultiTask;
-import me.ghosttypes.reaper.util.player.Interactions;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.mixininterface.IMinecraftClient;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -18,29 +16,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Mixin(
     value = {MinecraftClient.class},
     priority = 1001
 )
 public abstract class MincraftClientMixin implements IMinecraftClient {
-
-    @Inject(method = "getWindowTitle", at = @At("HEAD"), cancellable = true)
-    public void getWindowTitle(CallbackInfoReturnable<String> ci){
-        String title = "Reaper " + Reaper.VERSION;
-        if (Interactions.isDeveloper()) title = "Reaper " + Reaper.VERSION + " [Developer Edition]";
-        if (Interactions.isBetaUser()) title = "Reaper " + Reaper.VERSION + " [Beta Edition]";
-        ci.setReturnValue(title);
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
-    public void setAlternativeWindowIcon(Window window, InputStream inputStream1, InputStream inputStream2) throws IOException {
-        window.setIcon(
-            Reaper.class.getResourceAsStream("/assets/reaper/16.png"),
-            Reaper.class.getResourceAsStream("/assets/reaper/32.png")
-        );
-    }
 
     @Redirect(
         method = {"handleBlockBreaking"},
