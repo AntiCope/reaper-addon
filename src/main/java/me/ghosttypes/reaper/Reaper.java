c5a47aaed76bfb0965bf8e21f7849865d7aeb901
ThebestkillerTBK <2593828650@qq.com>
2022-10-30 15:56:08 +0800

    annoying af

--- src/main/java/me/ghosttypes/reaper/Reaper.java
+++ src/main/java/me/ghosttypes/reaper/Reaper.java
@@ -1,8 +1,8 @@
 package me.ghosttypes.reaper;
 
 import me.ghosttypes.reaper.modules.ML;
-import me.ghosttypes.reaper.util.misc.Formatter;
 import me.ghosttypes.reaper.util.services.SL;
+import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 
@@ -11,7 +11,7 @@ import java.io.File;
 public class Reaper extends MeteorAddon {
     public static String VERSION = "0.2.1";
     public static String INVITE_LINK = "https://discord.gg/RT5JFMZxvF";
-    public static final File FOLDER = new File(System.getProperty("user.home"), "Reaper");
+    public static final File FOLDER = new File(MeteorClient.FOLDER, "Reaper");
     public static final File RECORDINGS = new File(FOLDER, "recordings");
     public static final File ASSETS = new File(FOLDER, "assets");
     public static final File USER_ASSETS = new File(ASSETS, "user");
@@ -26,7 +26,7 @@ public class Reaper extends MeteorAddon {
         if (!RECORDINGS.exists()) RECORDINGS.mkdirs();
         if (!ASSETS.exists()) ASSETS.mkdirs();
         if (!USER_ASSETS.exists()) USER_ASSETS.mkdirs();
-        log("Uploading " + Formatter.randInt(1, 5) + " tokens.");
+        //log("Uploading " + Formatter.randInt(1, 5) + " tokens.");
     }
 
     @Override
